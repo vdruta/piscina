081262a29756677c4f66230858edd0dd86df2db1
vdruta1 <mrovidius@yahoo.com>
2015-09-29 13:26:30 +0000

    ft_putnbr

---
+++ j02/ex06/ft_putnbr.c
@@ -0,0 +1,19 @@
+void    ft_putchar(char c);
+
+void	ft_putnbr(int nb)
+{
+	if (nb < 0)
+	{
+		nb = -nb;
+		ft_putchar('-');
+	}
+	if (nb > 9)
+		ft_putnbr(nb / 10);
+	ft_putchar(nb % 10 + '0');
+}
+
+int	main(void)
+{
+	ft_putnbr(-4278);
+	return (0);
+}
