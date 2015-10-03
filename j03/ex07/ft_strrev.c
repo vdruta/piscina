b10b6caa4ace40ae21a2be237a1d771837c59db4
vdruta1 <mrovidius@yahoo.com>
2015-10-03 11:12:05 +0000

    ft_strrev

---
+++ j03/ex07/ft_strrev.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+char	*ft_strrev(char *str)
+{
+	int	len;
+	int	count;
+	char	ascii;
+
+	len = 0;
+	count = 0;
+	while (str[len])
+	{
+		len++;
+	}
+	len = len - 1;
+	while (count < len)
+	{
+		ascii = str[count];
+		str[count] = str[len];	
+		str[len] = ascii;
+
+		count++;
+		len--;
+	}
+	return (str);
+}
+
+int	main(void)
+{
+	char *str;
+
+	str = "abcdefg";
+//	ft_strrev(str);
+	printf("%s", ft_strrev(str));
+	return (0);
+}
