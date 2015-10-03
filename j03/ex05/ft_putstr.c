85f0f73d8e646f53f16d45936f98666d3f5aa675
vdruta1 <mrovidius@yahoo.com>
2015-10-03 09:49:48 +0000

    ft_putstr

---
+++ j03/ex05/ft_putstr.c
@@ -0,0 +1,22 @@
+void	ft_putchar(char c);
+
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int	main(void)
+{
+	char *str;
+
+	str = "abcdefg";
+	ft_putstr(str);
+	return (0);
+}
