ef14066c6c5d00087e94bc338bbda53e1c6a5329
vdruta1 <mrovidius@yahoo.com>
2015-10-05 10:19:38 +0000

    ft_sort_integer_table_test

--- j03/ex09/ft_sort_integer_table_test.c
+++ j03/ex09/ft_sort_integer_table_test.c
@@ -27,10 +27,24 @@ int	main(void)
 {
 	int tab[5] = {10, 7, 9, 0, 11};
 	int l;
+	int i;
 
+	i = 0;
 	l = 5;
 	printf("%d %d %d %d %d\n", tab[0], tab[1], tab[2], tab[3], tab[4]);
 	ft_sort_integer_table(tab, l);
-	printf("%d %d %d %d %d\n", tab[0], tab[1], tab[2], tab[3], tab[4]);
+	//printf("%d %d %d %d %d\n", tab[0], tab[1], tab[2], tab[3], tab[4]);
+	while (i < l)
+	{
+		printf("%d ", tab[i]);
+		i++;
+	}
+	printf("\n");
+	i = 0;
+	while (tab[i]) //do not appear?!?!?!
+	{
+		printf("%d ", tab[i]);
+		i++;
+	}
 	return (0);
 }
