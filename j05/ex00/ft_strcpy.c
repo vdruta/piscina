ba8daaa798dee710998b000058b2b770056d205a
vdruta1 <mrovidius@yahoo.com>
2015-10-07 11:55:37 +0000

    ft_strcpy

---
+++ j05/ex00/ft_strcpy.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	int i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+int	main(void)
+{
+	char dest[] = "dest";
+	char src[] = "Ala bala portocala";
+	printf("%s", ft_strcpy(dest, src));
+	return (0);
+}
