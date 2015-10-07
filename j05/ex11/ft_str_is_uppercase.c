0d2fe7c4f2a53456e62e42154c63ca13b55cb33d
vdruta1 <mrovidius@yahoo.com>
2015-10-07 20:51:53 +0000

    ft_str_is_uppercase

---
+++ j05/ex11/ft_str_is_uppercase.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int	ft_str_is_lowercase(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (!(str[i] >= 'A' && str[i] <= 'Z'))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	main(void)
+{
+	char str[] = "patruzecisidoi";
+
+	printf("%d", ft_str_is_lowercase(str));
+	return (0);
+}
