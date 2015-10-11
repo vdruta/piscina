402a8e4ddb05d71234a56a8395b4f6cddc3500c4
vdruta1 <mrovidius@yahoo.com>
2015-10-11 12:23:33 +0000

    ft_rev_params

---
+++ j06/ex03/ft_rev_params.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int i;
+	
+	i = 0;
+	while (str[i])
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	while (argc > 1)
+	{
+		ft_putstr(argv[argc - 1]);
+		ft_putchar('\n');
+		argc--;
+	}
+	return (0);
+}
