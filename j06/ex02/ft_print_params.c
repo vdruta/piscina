68ed7fd884732c36bd5e8610cda3b6e5471308a1
vdruta1 <mrovidius@yahoo.com>
2015-10-11 12:08:24 +0000

    ft_print_params

---
+++ j06/ex02/ft_print_params.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int i;
+
+	i = 1;
+	while (i < argc)
+	{
+		ft_putstr(argv[i]);
+		ft_putchar('\n');
+		i++;
+	}
+	return (0);
+}
