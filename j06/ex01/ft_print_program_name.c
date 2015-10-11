7f767260d8c2cfe77f3303121b18df94d1556eb4
vdruta1 <mrovidius@yahoo.com>
2015-10-11 11:54:53 +0000

    ft_print_program_name

---
+++ j06/ex01/ft_print_program_name.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	ft_putstr(argv[0]);
+	ft_putchar('\n');
+	return (0);
+}
