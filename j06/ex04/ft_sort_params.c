acca2f8ace0aab487ac1d87a4f95db0371547ebe
vdruta1 <mrovidius@yahoo.com>
2015-10-11 13:11:10 +0000

    ft_sort_params

---
+++ j06/ex04/ft_sort_params.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+void	ft_strswap(char **s1, char **s2)
+{
+	char *s3;
+
+	s3 = *s1;
+	*s1 = *s2;
+	*s2 = s3;
+}
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int i;
+	
+	i = 0;
+	while (s1[i] || s2[i])
+	{
+		if (s1[i] > s2[i])
+			return (s1[i] - s2[i]);
+		if (s1[i] < s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	int i;
+	int j;
+
+	i = 1;
+	while (i < argc)
+	{
+		j = i + 1;
+		while (j < argc)
+		{
+			if (ft_strcmp(argv[i], argv[j]) > 0)
+				ft_strswap(&argv[i], &argv[j]);
+			j++;
+		}
+		i++;
+	}
+	i = 1;
+	while (i < argc)
+	{
+		ft_putstr(argv[i]);
+		ft_putchar('\n');
+		i++;
+	}
+	return (0);
+}
