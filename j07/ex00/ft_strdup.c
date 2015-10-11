ad48602d58498d7438c6dc857eb5e73d9078a2a7
vdruta1 <mrovidius@yahoo.com>
2015-10-11 21:16:36 +0000

    ft_strdup

---
+++ j07/ex00/ft_strdup.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+char	*ft_strdup(char *src)
+{
+	char *dest;
+	int i;
+
+	dest = (char*)malloc(sizeof(dest));
+	i = 0;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+int	main(void)
+{
+	char str[] = "abcd";
+	printf("%s", ft_strdup(str));
+	return(0);
+}
